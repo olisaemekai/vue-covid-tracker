cbf859489bb839237a73cc5ab5efaf95f11a0ab5
Olisaemeka Isife <olisaemekai+fredrick@users.noreply.github.com>
2021-04-27 17:23:06 +0100

    first commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,26 @@
+<template>
+    <select @change="onChange()" v-model="selected" class="form-select mt-10 block w-full border p-2 rounded" id="">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :value="country.ID" :key="country.ID">
+            {{country.Country}}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data(){
+        return{
+            selected: 0
+        }
+    },
+    methods: {
+        onChange(){
+            const country = this.countries.find((item) => item.ID === this.selected)
+            return this.$emit('get-country', country)
+        }
+    }
+}
+</script>
